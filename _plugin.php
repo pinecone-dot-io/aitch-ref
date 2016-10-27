4b99cc94db5d61e1a2b9dfedef2aff908ab952bc
Eric Eaglstun <postpostmodern@gmail.com>
2016-10-27 11:53:15 -0500

    bump version

--- _plugin.php
+++ _plugin.php
@@ -3,9 +3,9 @@
 Plugin Name:	aitch-ref!
 Plugin URI:		http://wordpress.org/extend/plugins/aitch-ref/
 Description:	href junk. Requires PHP >= 5.3 and Wordpress >= 3.0
-Version:		0.9.5.1
-Author:			postpostmodern, pinecone-dot-io
-Author URI:		http://pinecone.io/
+Version:		0.9.6
+Author:			postpostmodern, pinecone-dot-website
+Author URI:		http://rack.and.pinecone.website/
 */
 
 register_activation_hook( __FILE__, create_function("", '$ver = "5.3"; if( version_compare(phpversion(), $ver, "<") ) die( "This plugin requires PHP version $ver or greater be installed." );') );
